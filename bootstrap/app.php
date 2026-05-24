114af543f348af620e5f24d82112af54d2c6e3d9
Bharath-Thiravium <bharath@thiravium.com>
2026-05-24 11:25:26 +0530

    `Added trustProxies middleware alias and configuration`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'subscription.full' => \App\Http\Middleware\EnforceFullSubscription::class,
             'auth'              => \App\Http\Middleware\Authenticate::class,
