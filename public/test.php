82564c6cf1f42e2b4e1bbca82c1667ee51ac0c54
saisabarish-athenas <saisabarishathenas@gmail.com>
2026-06-03 12:11:15 +0530

    refactor(.htaccess, test.php): remove RewriteBase and enhance test.php for better accessibility and response

--- public/test.php
+++ public/test.php
@@ -1,1 +1,12 @@
-<?php echo "PHP is working! PHP Version: " . PHP_VERSION; ?>
+<?php
+// This file should be accessible at /compliance/ce/public/test.php
+// If you can see this, .htaccess is working
+
+file_put_contents(dirname(__DIR__) . '/test-marker.txt', 'Test marker - ' . date('Y-m-d H:i:s') . "\n");
+
+echo json_encode([
+    'status' => 'OK',
+    'file' => __FILE__,
+    'message' => 'If you see this, public/test.php is being served',
+    'timestamp' => date('Y-m-d H:i:s'),
+]);
