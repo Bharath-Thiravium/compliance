3b8a6d145bfff712de475ce35cddcdabec219d0d
saisabarish-athenas <saisabarishathenas@gmail.com>
2026-06-03 11:16:11 +0530

    refactor(routes): remove diagnostics.php requirement from web routes

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-require __DIR__.'/diagnostics.php';
-
 Route::get('/login',     [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login',    [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout',   [AuthController::class, 'logout'])->name('logout');
